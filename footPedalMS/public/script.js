fac1f9de5fe4b3086e1b09ca2462067d4483fc52
Kornelia Papp <pappkornelia@outlook.com>
2019-03-25 16:37:11 +0100

    added new files

---
+++ footPedalMS/public/script.js
@@ -0,0 +1,94 @@
+var socket = null;
+var frozen = false;
+
+if (document.readyState != 'loading') ready();
+else document.addEventListener('DOMContentLoaded', ready);
+
+function ready() {
+  
+  document.getElementById('last').addEventListener('click', e=> {
+    frozen = !frozen;
+    document.getElementById('last').classList.toggle('frozen');
+  });
+  
+    
+  initWebsocket();
+}
+
+
+
+function onData(e) {
+  var accel = e.accel;
+  var accelGrav = e.accelGrav;
+  var rot = e.rot;
+  
+  //console.log(rot.alpha);
+  addedFunctionality(e);
+
+  //if (!frozen) showData(e);
+
+}
+
+function addedFunctionality(e){
+  colourPick(e.rot.gamma); 
+  // if (e.rot.gamma > 0 && e.rot.gamma < 30)
+  //   console.log("We did it!");
+}
+
+function rainbow(n) {
+  let c;
+  //scale down the min and max value to a usable value for the minor rotation the foot pedal will create
+  let u = scale(n, 0, 10, 0, 100); 
+ 
+
+  c = 'hsl(0, 0%, ' + u + '%)';
+
+  console.log(u);
+  return c;
+}
+
+function colourPick(i) {
+  document.body.style.background = rainbow(i);
+}
+
+
+//added function for scaling (map() in arduino) values
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+
+function initWebsocket() {
+  const url = 'ws://' + location.host + '/ws';
+  socket = new ReconnectingWebsocket(url);
+
+  // Connection has been established
+  socket.onopen = function(evt) {
+    console.log('Web socket opened: ' + url);
+  };
+
+  // Received a message
+  socket.onmessage = function(evt) {
+    // To convert text back to an object (if it was sent with 'sendObject'):
+    var o = JSON.parse(evt.data);
+    onData(o);
+  };
+
+}
+
+function showData(m) {
+  let html = 'accel';
+  html += '<table><tr><td>' + m.accel.x.toFixed(3) + '</td><td>' + m.accel.y.toFixed(3) + '</td><td>' + m.accel.z.toFixed(3) + '</tr></table>';
+  html += '</table>';
+  
+  html += 'rot';
+  html += '<table><tr><td>' + m.rot.alpha.toFixed(3) + '</td><td>' + m.rot.beta.toFixed(3) + '</td><td>' + m.rot.gamma.toFixed(3) + '</tr></table>';
+  
+  html += 'rotMotion';
+  html += '<table><tr><td>' + m.rotMotion.alpha.toFixed(3) + '</td><td>' + m.rotMotion.beta.toFixed(3) + '</td><td>' + m.rotMotion.gamma.toFixed(3) + '</tr></table>';
+  
+  html += 'accelGrav';
+  html += '<table><tr><td>' + m.accelGrav.x.toFixed(3) + '</td><td>' + m.accelGrav.y.toFixed(3) + '</td><td>' + m.accelGrav.z.toFixed(3) + '</tr></table>';
+  html += '</table>';
+  document.getElementById('last').innerHTML = html;
+}
